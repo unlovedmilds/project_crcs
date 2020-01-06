ffa4971e6bbfdcdbb6a429f04990a0586652582d
unlovedmilds <unloveddelavictoria@gmail.com>
2020-01-06 14:52:46 +0800

    Initial Commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/mdelavictoria/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/mdelavictoria/Downloads/Flutter-CRUD-API-Sample-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/mdelavictoria/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
